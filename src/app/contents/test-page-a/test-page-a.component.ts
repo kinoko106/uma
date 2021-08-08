1733724c7005d3bbd37957a985a09318dd16b70c
daichi takada <03daichi28@gmail.com>
2021-08-09 00:20:25 +0900

    a削除

--- src/app/contents/test-page-a/test-page-a.component.ts
+++ src/app/contents/test-page-a/test-page-a.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './test-page-a.component.html',
   styleUrls: ['./test-page-a.component.scss']
 })
+
+/** 暫定レース一覧画面 */
+/** 一覧表示(listbox)  */
+/** list選択したらそのレースの */
 export class TestPageAComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
